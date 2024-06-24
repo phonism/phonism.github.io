04744e851eaa072af6e4dc2f9b502b685a4e534c
phonism <sdu.phonism@gmail.com>
2024-06-24 11:47:37 +0800

    update

--- myworks/data.js
+++ myworks/data.js
@@ -4,24 +4,39 @@ var myData = {
         "tag": [],
         "bg": "#ADD8E6"
     },
-    "recommend":{
+    "recommend": {
         "city": "上海"
     },
     "gallery": [
         {
             "src": "myworks/photo1.jpg",
+            "w": 5190,
+            "h": 3460
+        },
+        {
+            "src": "myworks/photo2.jpg",
             "w": 5568,
             "h": 3712
         },
         {
-            "src": "myworks/photo2.jpg",
+            "src": "myworks/photo3.jpg",
             "w": 3585,
             "h": 2390
         },
         {
-            "src": "myworks/photo3.jpg",
-            "w": 5190,
-            "h": 3460
+            "src": "myworks/photo4.jpg",
+            "w": 3887,
+            "h": 2591
+        },
+        {
+            "src": "myworks/photo5.jpg",
+            "w": 3175,
+            "h": 2117
+        },
+        {
+            "src": "myworks/photo6.jpg",
+            "w": 4534,
+            "h": 3017
         }
     ]
 }
--- myworks/gen.py
+++ myworks/gen.py
@@ -1,11 +1,31 @@
 from PIL import Image
 import os
+import json
 
-for fl in os.listdir("./"):
+js = {
+    "designer": {
+        "name": "LuckyQueen's Photo Gallery",
+        "tag": [],
+        "bg": "#ADD8E6"
+    },
+    "recommend":{
+        "city": "上海"
+    },
+    "gallery": []
+}
+
+
+file_list = sorted(os.listdir("./"))
+
+for fl in file_list:
     if fl.find(".jpg") != -1:
         image = Image.open(fl)
         width, height = image.size
+        dic = {
+                "src": "myworks/" + fl,
+                "w": width,
+                "h": height,
+        }
+        js["gallery"].append(dic)
 
-        print(fl)
-        print(f"图像的宽度: {width} 像素")
-        print(f"图像的高度: {height} 像素")
+print("var myData = " + json.dumps(js, ensure_ascii=False, indent=4))
