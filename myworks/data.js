c9b501a490f09bb5da36edb1579a3b30afffd747
phonism <sdu.phonism@gmail.com>
2024-06-24 19:42:29 +0800

    update

--- myworks/data.js
+++ myworks/data.js
@@ -11,76 +11,76 @@ var myData = {
     },
     "gallery": [
         {
-            "src": "myworks/photo1.jpg",
-            "slt_src": "myworks/slt_photo1.jpg",
-            "w": 5190,
-            "h": 3460
-        },
-        {
-            "src": "myworks/photo10.jpg",
-            "slt_src": "myworks/slt_photo10.jpg",
-            "w": 2761,
-            "h": 1841
-        },
-        {
-            "src": "myworks/photo11.jpg",
-            "slt_src": "myworks/slt_photo11.jpg",
+            "src": "myworks/DSC_0392.jpg",
+            "slt_src": "myworks/slt_DSC_0392.jpg",
             "w": 5568,
             "h": 3712
         },
         {
-            "src": "myworks/photo12.jpg",
-            "slt_src": "myworks/slt_photo12.jpg",
-            "w": 3193,
-            "h": 2128
-        },
-        {
-            "src": "myworks/photo2.jpg",
-            "slt_src": "myworks/slt_photo2.jpg",
+            "src": "myworks/DSC_1645.jpg",
+            "slt_src": "myworks/slt_DSC_1645.jpg",
             "w": 5568,
             "h": 3712
         },
         {
-            "src": "myworks/photo3.jpg",
-            "slt_src": "myworks/slt_photo3.jpg",
-            "w": 3585,
-            "h": 2390
-        },
-        {
-            "src": "myworks/photo4.jpg",
-            "slt_src": "myworks/slt_photo4.jpg",
-            "w": 3887,
-            "h": 2591
-        },
-        {
-            "src": "myworks/photo5.jpg",
-            "slt_src": "myworks/slt_photo5.jpg",
+            "src": "myworks/DSC_2067.jpg",
+            "slt_src": "myworks/slt_DSC_2067.jpg",
             "w": 3175,
             "h": 2117
         },
         {
-            "src": "myworks/photo6.jpg",
-            "slt_src": "myworks/slt_photo6.jpg",
+            "src": "myworks/DSC_2559.jpg",
+            "slt_src": "myworks/slt_DSC_2559.jpg",
             "w": 4534,
             "h": 3017
         },
         {
-            "src": "myworks/photo7.jpg",
-            "slt_src": "myworks/slt_photo7.jpg",
-            "w": 3592,
-            "h": 5392
-        },
-        {
-            "src": "myworks/photo8.jpg",
-            "slt_src": "myworks/slt_photo8.jpg",
+            "src": "myworks/DSC_2607.jpg",
+            "slt_src": "myworks/slt_DSC_2607.jpg",
             "w": 5568,
             "h": 3712
         },
         {
-            "src": "myworks/photo9.jpg",
-            "slt_src": "myworks/slt_photo9.jpg",
+            "src": "myworks/DSC_2691.jpg",
+            "slt_src": "myworks/slt_DSC_2691.jpg",
+            "w": 3585,
+            "h": 2390
+        },
+        {
+            "src": "myworks/DSC_2745.jpg",
+            "slt_src": "myworks/slt_DSC_2745.jpg",
+            "w": 3887,
+            "h": 2591
+        },
+        {
+            "src": "myworks/DSC_2813.jpg",
+            "slt_src": "myworks/slt_DSC_2813.jpg",
+            "w": 5190,
+            "h": 3460
+        },
+        {
+            "src": "myworks/DSC_2829.jpg",
+            "slt_src": "myworks/slt_DSC_2829.jpg",
+            "w": 5568,
+            "h": 3712
+        },
+        {
+            "src": "myworks/DSC_5117.jpg",
+            "slt_src": "myworks/slt_DSC_5117.jpg",
+            "w": 3193,
+            "h": 2128
+        },
+        {
+            "src": "myworks/DSC_5711.jpg",
+            "slt_src": "myworks/slt_DSC_5711.jpg",
             "w": 4998,
             "h": 3332
+        },
+        {
+            "src": "myworks/DSC_5799.jpg",
+            "slt_src": "myworks/slt_DSC_5799.jpg",
+            "w": 2761,
+            "h": 1841
         }
     ]
 }
