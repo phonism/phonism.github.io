3b61c7fdf387047bf3118e72b282eb085a42321a
phonism <sdu.phonism@gmail.com>
2024-06-24 18:34:08 +0800

    update

--- myworks/data.js
+++ myworks/data.js
@@ -11,74 +11,74 @@ var myData = {
     },
     "gallery": [
         {
-            "src": "myworks/photo1.jpg",
-            "slt_src": "myworks/slt_photo1.jpg",
+            "src": "https://fastly.jsdelivr.net/gh/phonism/phonism.github.io/myworks/photo1.jpg",
+            "slt_src": "https://fastly.jsdelivr.net/gh/phonism/phonism.github.io/myworks/slt_photo1.jpg",
             "w": 5190,
             "h": 3460
         },
         {
-            "src": "myworks/photo10.jpg",
-            "slt_src": "myworks/slt_photo10.jpg",
+            "src": "https://fastly.jsdelivr.net/gh/phonism/phonism.github.io/myworks/photo10.jpg",
+            "slt_src": "https://fastly.jsdelivr.net/gh/phonism/phonism.github.io/myworks/slt_photo10.jpg",
             "w": 2761,
             "h": 1841
         },
         {
-            "src": "myworks/photo11.jpg",
-            "slt_src": "myworks/slt_photo11.jpg",
+            "src": "https://fastly.jsdelivr.net/gh/phonism/phonism.github.io/myworks/photo11.jpg",
+            "slt_src": "https://fastly.jsdelivr.net/gh/phonism/phonism.github.io/myworks/slt_photo11.jpg",
             "w": 5568,
             "h": 3712
         },
         {
-            "src": "myworks/photo12.jpg",
-            "slt_src": "myworks/slt_photo12.jpg",
+            "src": "https://fastly.jsdelivr.net/gh/phonism/phonism.github.io/myworks/photo12.jpg",
+            "slt_src": "https://fastly.jsdelivr.net/gh/phonism/phonism.github.io/myworks/slt_photo12.jpg",
             "w": 3193,
             "h": 2128
         },
         {
-            "src": "myworks/photo2.jpg",
-            "slt_src": "myworks/slt_photo2.jpg",
+            "src": "https://fastly.jsdelivr.net/gh/phonism/phonism.github.io/myworks/photo2.jpg",
+            "slt_src": "https://fastly.jsdelivr.net/gh/phonism/phonism.github.io/myworks/slt_photo2.jpg",
             "w": 5568,
             "h": 3712
         },
         {
-            "src": "myworks/photo3.jpg",
-            "slt_src": "myworks/slt_photo3.jpg",
+            "src": "https://fastly.jsdelivr.net/gh/phonism/phonism.github.io/myworks/photo3.jpg",
+            "slt_src": "https://fastly.jsdelivr.net/gh/phonism/phonism.github.io/myworks/slt_photo3.jpg",
             "w": 3585,
             "h": 2390
         },
         {
-            "src": "myworks/photo4.jpg",
-            "slt_src": "myworks/slt_photo4.jpg",
+            "src": "https://fastly.jsdelivr.net/gh/phonism/phonism.github.io/myworks/photo4.jpg",
+            "slt_src": "https://fastly.jsdelivr.net/gh/phonism/phonism.github.io/myworks/slt_photo4.jpg",
             "w": 3887,
             "h": 2591
         },
         {
-            "src": "myworks/photo5.jpg",
-            "slt_src": "myworks/slt_photo5.jpg",
+            "src": "https://fastly.jsdelivr.net/gh/phonism/phonism.github.io/myworks/photo5.jpg",
+            "slt_src": "https://fastly.jsdelivr.net/gh/phonism/phonism.github.io/myworks/slt_photo5.jpg",
             "w": 3175,
             "h": 2117
         },
         {
-            "src": "myworks/photo6.jpg",
-            "slt_src": "myworks/slt_photo6.jpg",
+            "src": "https://fastly.jsdelivr.net/gh/phonism/phonism.github.io/myworks/photo6.jpg",
+            "slt_src": "https://fastly.jsdelivr.net/gh/phonism/phonism.github.io/myworks/slt_photo6.jpg",
             "w": 4534,
             "h": 3017
         },
         {
-            "src": "myworks/photo7.jpg",
-            "slt_src": "myworks/slt_photo7.jpg",
+            "src": "https://fastly.jsdelivr.net/gh/phonism/phonism.github.io/myworks/photo7.jpg",
+            "slt_src": "https://fastly.jsdelivr.net/gh/phonism/phonism.github.io/myworks/slt_photo7.jpg",
             "w": 3592,
             "h": 5392
         },
         {
-            "src": "myworks/photo8.jpg",
-            "slt_src": "myworks/slt_photo8.jpg",
+            "src": "https://fastly.jsdelivr.net/gh/phonism/phonism.github.io/myworks/photo8.jpg",
+            "slt_src": "https://fastly.jsdelivr.net/gh/phonism/phonism.github.io/myworks/slt_photo8.jpg",
             "w": 5568,
             "h": 3712
         },
         {
-            "src": "myworks/photo9.jpg",
-            "slt_src": "myworks/slt_photo9.jpg",
+            "src": "https://fastly.jsdelivr.net/gh/phonism/phonism.github.io/myworks/photo9.jpg",
+            "slt_src": "https://fastly.jsdelivr.net/gh/phonism/phonism.github.io/myworks/slt_photo9.jpg",
             "w": 4998,
             "h": 3332
         }
